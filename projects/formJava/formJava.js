f9cda6440d6f81dc87cb9d6b1a475985fb26a1b5
Sheng <as1447as@gmail.com>
2024-11-18 11:39:54 -1000

    add finalpro

---
+++ projects/formJava/formJava.js
@@ -0,0 +1,35 @@
+document.getElementById('myForm').addEventListener('submit',function(event){
+    event.preventDefault();
+    alert("Form Submitted");
+    const first = document.getElementById('firstname').value; 
+    const last = document.getElementById('lastname').value;
+
+    if (!first){
+        alert("please type your name la")
+        return;
+    }
+
+    if (!last){
+        alert("please type your name la")
+        return;
+    }
+
+    const date = {
+        firstName: first,
+        lastName:  last
+    }
+
+    const xhr = new XMLHttpRequest();
+    xhr.open("POST", "submit.json", ture );
+    xhr.setRequestHeader("Content-Type","application/json;charset=UTF-8")
+    xhr.onreadystatechange = function (){
+        if (xhr.readyState ===4 && xhr.status ===200) {
+            const response = JSON.parse (xhr.responseText);
+            document.getElementById("message").innerHTML = response.message;
+        } else if (xhr.readyState ===4) {
+            alert('Error submitting form.')
+        }
+    };
+    xhr.send(JSON.stringify(data));
+    console.log(data);
+});
